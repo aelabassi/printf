a0c20789c1950358c2917f4d6ca5a7e10d184c3e
tagwa015 <tagwahassan15@gmail.com>
2023-10-16 20:34:33 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,65 +1,61 @@
 #include "main.h"
-
-
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
+ va_list args;
+ va_start(args, format);
 
-    int count = 0;
+ int count = 0;
 
-    while (format && *format != '\0')
+ while (format && *format != '\0')
+ {
+  if (*format == '%')
+  {
+   format++;
+
+   switch (*format)
+   {
+    case 'c':
     {
-        if (*format == '%')
-        {
-            format++;
-
-            switch(*format)
-            {
-                case 'c':
-                {
-                    // Print a character
-                    int c = va_arg(args, int);
-                    _putchar(c);
-                    count++;
-                    break;
-                }
-                case 's':
-                {
-                    // Print a string
-                    const char *s = va_arg(args, const char *);
-                    while (*s != '\0')
-                    {
-                        _putchar(*s);
-                        s++;
-                        count++;
-                    }
-                    break;
-                }
-                case '%':
-                {
-                    // Print a '%' character
-                    _putchar('%');
-                    count++;
-                    break;
-                }
-                format++;
-
-            }
-        }
-        else
-        {
-            // Print normal characters
-            _putchar(*format);
-            count++;
-            format++;
-        }
-
+     /* Print a character */
+     int c = va_arg(args, int);
+     _putchar(c);
+     count++;
+     break;
     }
+    case 's':
+    {
+     /* Print a string */
+     const char *s = va_arg(args, const char *);
+     while (*s != '\0')
+     {
+      _putchar(*s);
+      s++;
+      count++;
+     }
+     break;
+    }
+    case '%':
+    {
+     /* Print a '%' character */
+     _putchar('%');
+     count++;
+     break;
+    }
+   }
 
-    va_end(args);
+   format++;
+  }
+  else
+  {
+   /* Print normal characters */
+   _putchar(*format);
+   count++;
+   format++;
+  }
+ }
 
-    return count;
+ va_end(args);
+
+ return (count);
 }
-
 
