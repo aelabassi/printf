6c58563201238d7517424cd76e01d94e69798a5e
tagwa015 <tagwahassan15@gmail.com>
2023-10-18 15:11:52 -0700

    update handle_binary.c file and main.h

--- handle_binary.c
+++ handle_binary.c
@@ -2,28 +2,28 @@
 /**
  * print_binary - Prints the binary representation of a number
  */
-void print_binary(va_list , int n)
+int print_binary(va_list, int n)
 {
-    unsigned int x, i, sum;
-    unsigned int a[32];
-    int count;
+ unsigned int x, i, sum;
+ unsigned int a[32];
+ int count;
 
-    x = 32768; /* (2 ^ 15) */
-    a[0] = n / x;
-    for (i = 1; i < 32; i++)
-    {
-        x /= 2;
-        a[i] = (n / x) % 2;
-    }
-    for (i = 0, sum = 0, count = 0; i < 32; i++)
-    {
-        sum += a[i];
-        if (sum || i == 31)
-        {
-            char z = '0' + a[i];
-            printf("%c", z);
-            count++;
-        }
-    }
-    return count;
+ x = 32768; /* (2 ^ 15) */
+ a[0] = n / x;
+ for (i = 1; i < 32; i++)
+ {
+  x /= 2;
+  a[i] = (n / x) % 2;
+ }
+ for (i = 0, sum = 0, count = 0; i < 32; i++)
+ {
+  sum += a[i];
+  if (sum || i == 31)
+  {
+   char z = '0' + a[i];
+   putchar(z);
+   count++;
+  }
+ }
+ return (count);
 }
