43ae50a41d1233291a3664111163ed9d6b4e2d65
tagwa015 <tagwahassan15@gmail.com>
2023-10-18 13:17:42 -0700

    update handle_binary.c file

--- handle_binary.c
+++ handle_binary.c
@@ -1,31 +1,46 @@
 #include "main.h"
+/**
+ * print_binary - Prints the binary representation of a number
+ */
+void print_binary(unsigned int num)
+{
+ if (num > 1)
+  print_binary(num / 2);
+ putchar('0' + (num % 2));
+}int _printf(const char *format, ...)
+{
+ va_list args;
+ int printed_chars = 0;
+ const char *ptr;
 
-void print_binary(unsigned int num) {
-    if (num > 1) {
-        print_binary(num / 2);
-    }
-    _putchar('0' + (num % 2));
-}
+ va_start(args, format);
 
-void _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
+ for (ptr = format; *ptr != '\0'; ptr++)
+ {
+  if (*ptr != '%')
+  {
+   putchar(*ptr);
+   printed_chars++;
+  }
+  else
+  {
+   ptr++; /* Move past the '%' */
+   if (*ptr == 'b')
+   {
+    unsigned int num = va_arg(args, unsigned int);
+    print_binary(num);
+    printed_chars += sizeof(unsigned int) * 8;
+   }
+   else
+   {
+    putchar('%');
+    putchar(*ptr);
+    printed_chars += 2;
+   }
+  }
+ }
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;  // Move past the '%'
+ va_end(args);
 
-            // Handle the custom conversion specifier %b
-            if (*format == 'b') {
-                unsigned int num = va_arg(args, unsigned int);
-                print_binary(num);
-            }
-        } else {
-            putchar(*format);
-        }
-
-        format++;
-    }
-
-    va_end(args);
+ return printed_chars;
 }
