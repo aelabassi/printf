366b3e89f6c9b094206c04702a1898f57172b4df
tagwa015 <tagwahassan15@gmail.com>
2023-10-18 16:51:47 -0700

    update handle_binary.c file and main.h

--- handle_binary.c
+++ handle_binary.c
@@ -4,35 +4,35 @@
  */
 int print_binary(va_list args, int n)
 {
-    unsigned int x, i, sum;
-    unsigned int a[32];
-    int count;
+	unsigned int x, i, sum;
+	unsigned int a[32];
+	int count;
 
-    x = 32768; /* (2 ^ 15) */
-    a[0] = n / x;
+	x = 32768; /* (2 ^ 15) */
+	a[0] = n / x;
 
-    i = 1;
-    while (i < 32)
-    {
-        x /= 2;
-        a[i] = (n / x) % 2;
-        i++;
-    }
+	i = 1;
+	while (i < 32)
+	{
+		x /= 2;
+		a[i] = (n / x) % 2;
+		i++;
+	}
 
-    i = 0;
-    sum = 0;
-    count = 0;
-    while (i < 32)
-    {
-        sum += a[i];
-        if (sum || i == 31)
-        {
-            char z = '0' + a[i];
-            _putchar(z);
-            count++;
-        }
-        i++;
-    }
+	i = 0;
+	sum = 0;
+	count = 0;
+	while (i < 32)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+			_putchar(z);
+			count++;
+		}
+		i++;
+	}
 
-    return (count);
+	return (count);
 }
